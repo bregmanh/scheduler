6d8daa4640e35edeed937175fda5cfb7dd87a212
Hannah Bregman <bregmanhannah@gmail.com>
2020-07-08 20:05:37 -0400

    organized code a bit better

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import "./Application.scss";
 
-const {getAppointmentsForDay} = require('../helpers/selectors')
+const { getAppointmentsForDay } = require('../helpers/selectors')
 const axios = require('axios');
 
 
@@ -13,31 +13,20 @@ export default function Application(props) {
     days: [],
     appointments: {}
   });
+
   const setDay = day => setState({ ...state, day });
   const appointments = getAppointmentsForDay(state, state.day)
+
   useEffect(() => {
-    
     Promise.all([
       axios.get('http://localhost:8001/api/days'),
       axios.get('http://localhost:8001/api/appointments')
     ]).then((all) => {
       const [daysResponse, appointmentsResponse] = all;
-
       setState(prev => ({ days: daysResponse.data, appointments: appointmentsResponse.data }));
-      
-    
-    
       console.log(daysResponse, appointmentsResponse);
     });
-
-
-      
-    
-    
-
-    
   }, [])
-
 
   return (
     <main className="layout">
